f205caae84498a8d49ccd14697bed8e3af433ae3
Minwoo <mwkim0919@gmail.com>
2015-06-17 16:45:54 -0700

    fixing scrollable table

--- van/static/vancouver/js/modal.js
+++ van/static/vancouver/js/modal.js
@@ -2,3 +2,18 @@ $(".modal-wide").on("show.bs.modal", function() {
 	var height = $(window).height() - 200;
 	$(this).find(".modal-body").css("max-height", height);
 });
+
+// var tableOffset = $("#table-1").offset().top;
+// var $header = $("#table-1 > thead").clone();
+// var $fixedHeader = $("#header-fixed").append($header);
+
+// $(window).bind("scroll", function() {
+//     var offset = $(this).scrollTop();
+
+//     if (offset >= tableOffset && $fixedHeader.is(":hidden")) {
+//         $fixedHeader.show();
+//     }
+//     else if (offset < tableOffset) {
+//         $fixedHeader.hide();
+//     }
+// });
